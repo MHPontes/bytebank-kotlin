9f6047057faa60a32de4a906e49877a6929eec38
Mateus Pontes <mateushspontes@gmail.com>
2023-03-20 10:25:14 -0300

    Iniciando com Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println ("Bem vindo ao Bytebank")
+
+  //  Você pode definir essa variável tanto como mutável (var) como imutável (val).
+
+    val titular: String = "Alex"
+    val numeroConta: Int = 1000
+    var saldo = 100.0
+    saldo = 200.0
+    saldo += 50
+    println ("Titular $titular")
+    println ("numeroConta $numeroConta ")
+    println ("saldo $saldo")
+
+}
