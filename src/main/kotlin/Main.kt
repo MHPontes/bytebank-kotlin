dba9dec3bbbcebe471e38a6a66bfa47ae3e4d61d
Mateus Pontes <mateushspontes@gmail.com>
2023-03-21 09:42:45 -0300

    Aprendendo sobre if e when

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,10 +6,25 @@ fun main() {
     val titular: String = "Alex"
     val numeroConta: Int = 1000
     var saldo = 100.0
-    saldo = 200.0
-    saldo += 50
+//    saldo = 200.0
+//    saldo += 50
     println ("Titular $titular")
     println ("numeroConta $numeroConta ")
     println ("saldo $saldo")
+
+
+//    if (saldo > 0.0) {
+//        println ("A conta é positiva.")
+//    } else if (saldo == 0.0) {
+//        println("A conta é neutra.")
+//    } else {
+//        println("A conta é negativa!")
+//    }
+
+    when {
+        saldo > 0.0 ->  println ("A conta é positiva.")
+        saldo == 0.0 ->  println("A conta é neutra.")        //Substiui o uso dos ifs acima
+        else ->  println("A conta é negativa!")
+    }
 
 }
